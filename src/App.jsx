0e6258b0a78461a0a321c34c1bd4f2c0101b7a72
Radwa Khaled <radwaabouzed@gmail.com>
2025-09-06 21:32:53 +0300

    add header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,20 @@
 
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import './Header.css'
 import './App.css'
-import MyFirstComponent from './components/MyFirstComponent'
+import HeaderComponent from './components/HeaderComponent'
 
 function App() {
   
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      
-      <MyFirstComponent/>
-
-    </>
-  )
+  <div>
+        <div>  
+           <nav>
+               <header><HeaderComponent/></header>  
+           </nav>
+        </div>
+ </div>
+  );
 }
 
 export default App
