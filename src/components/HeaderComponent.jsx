32d16d263883e553aa9e5c321c11531cb4edcd38
Radwa Khaled <radwaabouzed@gmail.com>
2025-09-06 21:34:10 +0300

    add header component

---
+++ src/components/HeaderComponent.jsx
@@ -0,0 +1,15 @@
+
+
+export default function HeaderComponent()
+{
+
+const header ={
+    name: "Tarmeez Acadamy",
+};
+    return(
+        <div>
+           <h1>{header.name}</h1>
+        </div>
+
+    );
+}
