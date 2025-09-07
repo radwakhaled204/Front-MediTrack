22b9dd8eb5ce902d974cc7fd1d5c82a6eef25455
Radwa Khaled <radwaabouzed@gmail.com>
2025-09-07 21:57:05 +0300

    add new component

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@
 import './Header.css'
 import './App.css'
 import HeaderComponent from './components/HeaderComponent'
+import PostComponent from './components/PostComponent'
 
+ import SideBarComponent from './components/SideBarComponent'
 function App() {
   
 
@@ -10,9 +12,32 @@ function App() {
   <div>
         <div>  
            <nav>
-               <header><HeaderComponent/></header>  
+               <header><HeaderComponent/></header>                 
            </nav>
         </div>
+      
+  <div className="body">
+        <div className="posts-container">
+          <PostComponent/>
+          <PostComponent  />
+          <PostComponent />
+          <PostComponent />
+        </div>
+        
+           <div className="sidebar-container">
+            <div className={"sidebar"}>
+        <SideBarComponent/>
+        <SideBarComponent/>
+        <SideBarComponent/>
+        <SideBarComponent/>
+
+        </div> 
+        </div>
+      </div>     
+      
+       
+  
+    
  </div>
   );
 }
