5429cfb6306858256792f4f1d40dc5ab2bb0e497
Radwa Khaled <radwaabouzed@gmail.com>
2025-09-07 21:57:39 +0300

    add SideBarComponent

---
+++ src/components/SideBarComponent.jsx
@@ -0,0 +1,12 @@
+
+import '../Header.css'
+export default function SideBarComponent()
+{
+    return (
+     <div>
+     <button>clickme</button>
+     <button>clickme</button>
+     <button>clickme</button>
+     </div>
+    );
+}
