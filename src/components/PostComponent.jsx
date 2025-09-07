b3ca5425138ca04ff050def6dbe6675577cb37b2
Radwa Khaled <radwaabouzed@gmail.com>
2025-09-07 21:57:29 +0300

    add PostComponent

---
+++ src/components/PostComponent.jsx
@@ -0,0 +1,12 @@
+
+import '../Header.css'
+export default function PostComponent()
+{
+    return (
+     <div  className={"post"}>
+        <h2>This is the post title</h2> 
+        <hr />    
+        <p>This is the post body</p>
+     </div>
+    );
+}
